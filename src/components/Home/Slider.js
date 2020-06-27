b20f1322a7fb1abb463161cdc2b2411dc8933217
Daniel Arango <you@example.comarangomonsalve.daniel@gmail.com>
2020-06-27 08:48:56 -0500

    Testimonials

---
+++ src/components/Home/Slider.js
@@ -0,0 +1,120 @@
+import React from "react"
+import imagen1 from "../../images/slide01_800.jpg"
+
+const Slider = () => {
+  return (
+    <section className="page_slider slider-1">
+      <div className="flexslider">
+        <ul className="slides">
+          <li className="ds cover-image s-overlay mobile-overlay text-center text-md-left">
+            <img src={imagen1} alt="img"/>
+            <div className="container">
+              <div className="row">
+                <div className="col-12">
+                  <div className="intro_layers_wrapper">
+                    <div className="intro_layers">
+                      <div className="intro_layer" data-animation="fadeInRight">
+                        <div className="divider-60 divider-xl-80"></div>
+                        <div className="d-inline-block">
+                          <h2 className="intro_featured_word">
+                            Ultimate <br/>
+                            Guide
+                          </h2>
+                          <p>
+                            There are 12 signs of the zodiac, each one taking up 30 degrees in your birth chart,
+                            manking a complete 360 degree circle
+                          </p>
+                        </div>
+                      </div>
+                      <div className="intro_layer" data-animation="fadeInUp">
+                        <a href="./about.html" className="btn btn-maincolor btn-slider btn-medium">Let’s Begin</a>
+                      </div>
+                    </div>
+                    {/*// <!-- eof .intro_layers -->*/}
+                  </div>
+                  {/*// <!-- eof .intro_layers_wrapper -->*/}
+                </div>
+                {/*// <!-- eof .col-* -->*/}
+              </div>
+              {/*// <!-- eof .row -->*/}
+            </div>
+            {/*// <!-- eof .container-fluid -->*/}
+          </li>
+          <li className="ds cover-image s-overlay mobile-overlay text-center text-md-left">
+            <img src="images/slide02_800.jpg" alt="img"/>
+            <div className="container">
+              <div className="row">
+                <div className="col-12">
+                  <div className="intro_layers_wrapper">
+                    <div className="intro_layers">
+                      <div className="intro_layer" data-animation="fadeInRight">
+                        <div className="divider-60 divider-xl-80"></div>
+                        <div className="d-inline-block">
+                          <h2 className="intro_featured_word">
+                            Ultimate <br/>
+                            Guide
+                          </h2>
+                          <p>
+                            There are 12 signs of the zodiac, each one taking up 30 degrees in your birth chart,
+                            manking a complete 360 degree circle
+                          </p>
+                        </div>
+                      </div>
+                      <div className="intro_layer" data-animation="fadeInUp">
+                        <a href="./about.html" className="btn btn-maincolor btn-slider btn-medium">Let’s Begin</a>
+                      </div>
+                    </div>
+                    {/*// <!-- eof .intro_layers -->*/}
+                  </div>
+                  {/*{!--eof.intro_layers_wrapper--}*/}
+                </div>
+                {/*// <!-- eof .col-* -->*/}
+              </div>
+              {/*// <!-- eof .row -->*/}
+            </div>
+            {/*// <!-- eof .container-fluid -->*/}
+          </li>
+          <li className="ds cover-image s-overlay mobile-overlay text-center text-md-left">
+            <img src="images/slide03_800.jpg" alt="img"/>
+            <div className="container">
+              <div className="row">
+                <div className="col-12">
+                  <div className="intro_layers_wrapper">
+                    <div className="intro_layers">
+                      <div className="intro_layer" data-animation="fadeInRight">
+                        <div className="divider-60 divider-xl-80"></div>
+                        <div className="d-inline-block">
+                          <h2 className="intro_featured_word">
+                            Ultimate <br/>
+                            Guide
+                          </h2>
+                          <p>
+                            There are 12 signs of the zodiac, each one taking up 30 degrees in your birth chart,
+                            manking a complete 360 degree circle
+                          </p>
+                        </div>
+                      </div>
+                      <div className="intro_layer" data-animation="fadeInUp">
+                        <a href="./about.html" className="btn btn-maincolor btn-slider btn-medium">Let’s Begin</a>
+                      </div>
+                    </div>
+                    {/*// <!-- eof .intro_layers -->*/}
+                  </div>
+                  {/*// <!-- eof .intro_layers_wrapper -->*/}
+                </div>
+                {/*// <!-- eof .col-* -->*/}
+              </div>
+              {/*// <!-- eof .row -->*/}
+            </div>
+            {/*// <!-- eof .container-fluid -->*/}
+          </li>
+        </ul>
+      </div>
+      {/*// <!-- eof flexslider -->*/}
+    </section>
+
+
+  )
+}
+
+export default Slider
