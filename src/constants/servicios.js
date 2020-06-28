27b5368044b0817d8cfb8b6ec73b161cc2962980
Daniel Arango <you@example.comarangomonsalve.daniel@gmail.com>
2020-06-27 21:41:18 -0500

    Links Servicios

---
+++ src/constants/servicios.js
@@ -0,0 +1,41 @@
+import React from "react"
+import {GiPokerHand} from "react-icons/all"
+import {GiCrystalBall} from "react-icons/all"
+import {GiMagicPalm} from "react-icons/all"
+import {GiMagicPotion} from "react-icons/all"
+import {RiMoonClearLine} from "react-icons/all"
+import {GiRuneStone} from "react-icons/all"
+
+
+export default [
+  {
+    icon: <GiPokerHand/>,
+    title: "Natal Chart",
+    text: "Sadipscing elitr sed diam nonumy eirmod."
+  },
+  {
+    icon: <GiCrystalBall/>,
+    title: "Transit Chart",
+    text: "Sadipscing elitr sed diam nonumy eirmod."
+  },
+  {
+    icon: <GiMagicPalm/>,
+    title: "Medical Reading",
+    text: "Sadipscing elitr sed diam nonumy eirmod."
+  },
+  {
+    icon: <GiMagicPotion/>,
+    title: "Couple's Reading",
+    text: "Sadipscing elitr sed diam nonumy eirmod."
+  },
+  {
+    icon: <RiMoonClearLine/>,
+    title: "Career Chart",
+    text: "Sadipscing elitr sed diam nonumy eirmod."
+  },
+  {
+    icon: <GiRuneStone/>,
+    title: "Birt Stone",
+    text: "Sadipscing elitr sed diam nonumy eirmod."
+  },
+]
